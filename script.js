9fe221ddc9c96824c8d634b7f65c4531c6dbe898
mdatiq <114382625+developeratiq@users.noreply.github.com>
2022-09-27 20:25:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("code is running---");
+document.querySelector('.cross').style.display= 'none';
+document.querySelector('.menu').addEventListener("click", ()=>{
+ document.querySelector('.sidebar').classList.toggle('sidebargo')
+
+
+})
